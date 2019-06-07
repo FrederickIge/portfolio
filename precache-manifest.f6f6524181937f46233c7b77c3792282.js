a75adad769b863377231ee2092a6d50623f69c05
FrederickIge <duelarm@gmail.com>
2019-06-07 10:17:31 -0400

    Updates

---
+++ precache-manifest.f6f6524181937f46233c7b77c3792282.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1171e20c35d62c5c06b252d36502b670",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "902fc9ebd4da65606391",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5b367bfa753a19929dd1",
+    "url": "/portfolio/static/css/main.bfe123e2.chunk.css"
+  },
+  {
+    "revision": "902fc9ebd4da65606391",
+    "url": "/portfolio/static/js/2.cb87d9fd.chunk.js"
+  },
+  {
+    "revision": "5b367bfa753a19929dd1",
+    "url": "/portfolio/static/js/main.db2d28f6.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b79b18e2682351d7ff3a243fdd6f53bb",
+    "url": "/portfolio/static/media/bitclaims.b79b18e2.JPG"
+  },
+  {
+    "revision": "d68c76be343556628179b985183eabf8",
+    "url": "/portfolio/static/media/delish.d68c76be.jpg"
+  },
+  {
+    "revision": "f1f0338bbd7a86b10fba92bedc3fa509",
+    "url": "/portfolio/static/media/dev.f1f0338b.png"
+  },
+  {
+    "revision": "10e30fd15ebd459e710a11b9a51a05c8",
+    "url": "/portfolio/static/media/graphql.10e30fd1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "98c8cf5e0f7b4f8c1b1ea2c105e53036",
+    "url": "/portfolio/static/media/mobx.98c8cf5e.png"
+  },
+  {
+    "revision": "36700bee9a1831f2691165d291d62d78",
+    "url": "/portfolio/static/media/node.36700bee.png"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/portfolio/static/media/pg.ea92d658.png"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/portfolio/static/media/react.82a318cc.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/portfolio/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "18a93dff31b8e929f2d874476b78dabd",
+    "url": "/portfolio/static/media/university.18a93dff.JPG"
+  }
+]);
